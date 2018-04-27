35130c6b0e1c5c15fadbec60e271e3c2c307c308
Doug Lokrantz <doug.lokrantz@gmail.com>
2018-04-27 18:13:12 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -33,5 +33,9 @@ class TicTacToe
   def position_taken?(location)
     @board[location] != "" && @board[location] != " "
   end
+  
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken? @board[index]
+  end
     
 end
