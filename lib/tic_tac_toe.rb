6114f6302aa9c80caeb8ddd894b081e479ee09b0
Doug Lokrantz <doug.lokrantz@gmail.com>
2018-04-27 18:26:47 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,6 +52,23 @@ class TicTacToe
     turn_count.even? ? "X" : "O"
   end 
   
-  
+  def turn
+    puts "Please enter 1-9:"
+    i = gets.strip
+    index = input_to_index(i)
+    m = valid_move?(index)
+    if m == true
+      move(index, current_player)
+    else m == false
+      until m == true
+        puts "Sorry, that was an invalid move. Please enter 1-9:"
+        display_board
+        i = gets.strip
+        index = input_to_index(i)
+        m = valid_move?(index)
+        move(index, current_player)
+      end
+    end
+  end
   
 end
