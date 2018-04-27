0ac09b199fab865ea6640dda9ac9def0bf801667
Doug Lokrantz <doug.lokrantz@gmail.com>
2018-04-27 17:48:55 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -6,6 +6,7 @@ class TicTacToe
   def WIN_COMBINATIONS [
     [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]
     ]
+  end
   
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
